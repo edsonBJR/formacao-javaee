2b6eed120dce9a0335ad78725730c31553b30e33
edsonBJR <ebjr.tux@gmail.com>
2021-03-02 17:58:40 -0300

    Utilizando um padrão de projeto o DAO.

--- projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaMediaDiariaDasMovimentacoes.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaMediaDiariaDasMovimentacoes.java
@@ -1,24 +1,19 @@
 package br.com.alura.jpa.testes;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import java.util.List;
+
+import br.com.alura.jpa.modelo.MediaComData;
+import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
 
 public class TestaMediaDiariaDasMovimentacoes {
 
 	public static void main(String[] args) {
-
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-		EntityManager em = emf.createEntityManager();
-
-		String jpql = "select avg(m.valor) from Movimentacao m";
-
-		TypedQuery<Double> query = em.createQuery(jpql, Double.class);
-
-		Double mediaDasMovimentacoes = query.getSingleResult();
 		
-		System.out.println("A Soma das Movimentações é: " + mediaDasMovimentacoes);
+		List<MediaComData> mediaDasMovimentacoes = new MovimentacaoDao().getMediaDiariaDasMovimentacoes();
+		
+		for (MediaComData resultado : mediaDasMovimentacoes) {
+			System.out.println("A média das Movimentações do dia " + resultado.getDia() + "/" + resultado.getMes() + " é: " + resultado.getValor());			
+		}
 	}
 
 }
--- projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
@@ -1,32 +1,30 @@
 package br.com.alura.jpa.testes;
 
-import java.math.BigDecimal;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
 
 public class TestaSomaDasMovimentacoes {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-		EntityManager em = emf.createEntityManager();
+		MovimentacaoDao dao = new MovimentacaoDao();
+		System.out.println("A Soma das Movimentações é: " + dao.getSomaDasMovimentacoes());
+
+
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+//		EntityManager em = emf.createEntityManager();
+//		
+//		// Aqui estamos buscando a soma
+//		// String jpql = "select sum(m.valor) from Movimentacao m";
+//		// Aqui estamos buscando a média
+//		String jpql = "select avg(m.valor) from Movimentacao m";
+//		
+//		// Aqui o tipo correto para armazenar a soma das movimentações
+//		// TypedQuery<BigDecimal> query = em.createQuery(jpql, BigDecimal.class);
+//		
+//		// Aqui o tipo correto para armazenar a média das movimentações
+//		TypedQuery<Double> query = em.createQuery(jpql, Double.class);
+//		
+//		// BigDecimal somaDasMovimentacoes = query.getSingleResult();
+//		Double mediaDasMovimentacoes = query.getSingleResult();
 		
-		// Aqui estamos buscando a soma
-		// String jpql = "select sum(m.valor) from Movimentacao m";
-		// Aqui estamos buscando a média
-		String jpql = "select avg(m.valor) from Movimentacao m";
-		
-		// Aqui o tipo correto para armazenar a soma das movimentações
-		// TypedQuery<BigDecimal> query = em.createQuery(jpql, BigDecimal.class);
-		
-		// Aqui o tipo correto para armazenar a média das movimentações
-		TypedQuery<Double> query = em.createQuery(jpql, Double.class);
-		
-		// BigDecimal somaDasMovimentacoes = query.getSingleResult();
-		Double mediaDasMovimentacoes = query.getSingleResult();
-		
-		System.out.println("A Soma das Movimentações é: " + mediaDasMovimentacoes);
 	}
 }
