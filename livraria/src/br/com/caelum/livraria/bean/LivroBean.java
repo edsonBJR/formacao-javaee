9d5db688e9651953483edcdc477918e02ea794bc
edsonBJR <ebjr.tux@gmail.com>
2021-03-07 22:50:39 -0300

    Removendo e atualizando dados do Livro

--- livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -58,7 +58,27 @@ public class LivroBean {
 			return;
 		}
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		if(this.livro.getId() == null) {
+			new DAO<Livro>(Livro.class).adiciona(this.livro);			
+		} else {
+			new DAO<Livro>(Livro.class).atualiza(this.livro);
+		}
+		
+		this.livro = new Livro();
+	}
+	
+	public void remover(Livro livro) {
+		System.out.println("Removendo Livro");
+		new DAO<Livro>(Livro.class).remove(livro);
+	}
+	
+	public void removerAutorDoLivro(Autor autor) {
+		this.livro.removeAutor(autor);
+	}
+	
+	public void carregar(Livro livro) {
+		System.out.println("Carregando livro");
+		this.livro = livro;
 	}
 	
 	public String formAutor() {
--- livraria/src/br/com/caelum/livraria/modelo/Livro.java
+++ livraria/src/br/com/caelum/livraria/modelo/Livro.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
@@ -26,7 +27,7 @@ public class Livro implements Serializable{
 	@Temporal(TemporalType.DATE)
 	private Calendar dataLancamento = Calendar.getInstance();
 
-	@ManyToMany
+	@ManyToMany(fetch=FetchType.EAGER)// Carregamento Lazy, quando se carrega o Livro não se carrega os Autores
 	private List<Autor> autores = new ArrayList<Autor>();
 
 	public List<Autor> getAutores() {
@@ -78,6 +79,10 @@ public class Livro implements Serializable{
 
 	public void setDataLancamento(Calendar dataLancamento) {
 		this.dataLancamento = dataLancamento;
+	}
+
+	public void removeAutor(Autor autor) {
+		this.autores.remove(autor);
 	}
 
 }
