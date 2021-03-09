763af9c45f8a20692d4072cbd94732e04d4db5a5
edsonBJR <ebjr.tux@gmail.com>
2021-03-09 15:40:54 -0300

    Projeto finalizado, com escolha de temas de frontend.

--- livraria-primefaces/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria-primefaces/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -17,31 +17,26 @@ public class LoginBean {
 	public Usuario getUsuario() {
 		return usuario;
 	}
-
+	
 	public String efetuaLogin() {
-		System.out.println("Fazendo login do usuário "
-				+ this.usuario.getEmail());
+		System.out.println("fazendo login do usuario " + this.usuario.getEmail());
 		
 		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDao().existe(this.usuario);
-		
-		if (existe) {
-
-			context.getExternalContext().getSessionMap()
-					.put("usuarioLogado", this.usuario);
-
+		if(existe ) {
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
-
-		return null;
+		
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+		
+		return "login?faces-redirect=true";
 	}
 	
-    public String deslogar() {
-
-        FacesContext context = FacesContext.getCurrentInstance();
-        context.getExternalContext().getSessionMap().remove("usuarioLogado");
-
-        return "login?faces-redirect=true";
-    }
-
+	public String deslogar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return "login?faces-redirect=true";
+	}
 }
--- livraria-primefaces/src/br/com/caelum/livraria/util/Autorizador.java
+++ livraria-primefaces/src/br/com/caelum/livraria/util/Autorizador.java
@@ -13,30 +13,29 @@ public class Autorizador implements PhaseListener {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public void afterPhase(PhaseEvent event) {
+	public void afterPhase(PhaseEvent evento) {
 
-		FacesContext context = event.getFacesContext();
+		FacesContext context = evento.getFacesContext();
 		String nomePagina = context.getViewRoot().getViewId();
-
+	
 		System.out.println(nomePagina);
-
-		if ("/login.xhtml".equals(nomePagina)) {
+		
+		if("/login.xhtml".equals(nomePagina)) {
 			return;
 		}
-
-		Usuario usuarioLogado = (Usuario) context.getExternalContext()
-				.getSessionMap().get("usuarioLogado");
+		
+		Usuario usuarioLogado = (Usuario) context.getExternalContext().getSessionMap().get("usuarioLogado");
 		
 		if(usuarioLogado != null) {
 			return;
 		}
 		
+		//redirecionamento para login.xhtml
+		
 		NavigationHandler handler = context.getApplication().getNavigationHandler();
 		handler.handleNavigation(context, null, "/login?faces-redirect=true");
-		
 		context.renderResponse();
-		
-	}
+	} 
 
 	@Override
 	public void beforePhase(PhaseEvent event) {
